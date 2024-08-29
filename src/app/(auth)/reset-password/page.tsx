68dbc41c76a9d4e7fd8707556588797258b7e5c7
daniel-w1 <wossendaniel7@gmail.com>
2024-08-29 20:35:32 +0300

    fix build issues

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ResetPasswordUI from './reset-password-ui'
+import { Suspense } from 'react'
+import { Loader2 } from 'lucide-react'
 
 const ResetPassword = () => {
   return (
-    <ResetPasswordUI />
+    <Suspense fallback={<div className='flex items-center justify-center min-h-screen'><Loader2 className='animate-spin' /></div>}>
+      <ResetPasswordUI />
+    </Suspense>
   )
 }
 
