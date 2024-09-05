2ca0a081387827dad8879d985fae93fc8b302c8e
daniel-w1 <wossendaniel7@gmail.com>
2024-09-05 11:31:47 +0300

    add suspense boundary

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import SearchUI from './components/search-ui'
+import { Loader2 } from 'lucide-react'
 
 const SearchPage = () => {
     return (
-        <SearchUI />
+        <Suspense fallback={<div className='flex items-center justify-center min-h-screen'><Loader2 className='animate-spin' /></div>}>
+            <SearchUI />
+        </Suspense>
     )
 }
 
